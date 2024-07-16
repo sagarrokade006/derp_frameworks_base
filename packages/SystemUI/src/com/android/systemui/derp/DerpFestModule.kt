73f46ce4f8722a1c53b92d84fc0645b79ddc0bb8
Pranav Vashi <neobuddy89@gmail.com>
2024-07-16 11:25:21 +0200

    SystemUI: Add Weather tile based on OmniJaws client * Initial code based on 11.0 commit crdroidandroid/android_frameworks_base@9773423 * Removed detailed adapter - deprecated now. :( * Simplify code and strings * Use location as tile label when enabled * handleClick will open settings when disabled, will open weather app when enabled * handleLongClick will always open settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/derp/DerpFestModule.kt
+++ packages/SystemUI/src/com/android/systemui/derp/DerpFestModule.kt
@@ -35,6 +35,7 @@ import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VPNTetheringTile
 import com.android.systemui.qs.tiles.VpnTile
+import com.android.systemui.qs.tiles.WeatherTile
 import com.android.systemui.qs.tiles.WifiTile
 
 import dagger.Binds
@@ -151,6 +152,12 @@ interface DerpFestModule {
     @IntoMap
     @StringKey(VpnTile.TILE_SPEC)
     fun bindVpnTile(vpnTile: VpnTile): QSTileImpl<*>
+
+    /** Inject WeatherTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(WeatherTile.TILE_SPEC)
+    fun bindWeatherTile(weatherTile: WeatherTile): QSTileImpl<*>
 
     /** Inject WifiTile into tileMap in QSModule */
     @Binds
